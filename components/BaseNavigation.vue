995b7f15d1d65c9f9e76fae7760c636ddc77b7fc
jbiddulph <john.mbiddulph@gmail.com>
2024-10-23 11:36:45 +0100

    toggle dark and light

--- components/BaseNavigation.vue
+++ components/BaseNavigation.vue
@@ -1,5 +1,13 @@
 <template>
-  <header class="py-4 border-b">
+  <header class="py-4 border-b flex flex-row">
+    <button @click="toggleTheme" class="ml-4">
+      <template v-if="isDarkMode">
+        <UIcon name="mdi:moon-waning-crescent" color="pistachio" />
+      </template>
+      <template v-else>
+        <UIcon name="mdi:white-balance-sunny" color="pistachio" />
+      </template>
+    </button>
     <nav class="flex container items-center">
       <NuxtLink to="/" title="Caravans For Sale" class="flex gap-1 items-center">
         <img src="/public/img/home-logo-colour-svg_0.svg" alt="Caravans For Sale" class="w-[120px] md:w-full">
@@ -16,5 +24,14 @@
 </template>
 
 <script setup lang="ts">
+const isDarkMode = ref(false);
 
+const toggleTheme = () => {
+  isDarkMode.value = !isDarkMode.value;
+  if (isDarkMode.value) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+};
 </script>
